412fb3df08ea89a1f26390779aa6030511a040ef
ac2393921 <ac2393921@gmail.com>
2023-12-06 23:50:16 +0900

    feat: NavBarの作成

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,6 +1,8 @@
+import Navbar from '@/components/Navbar';
 import useCurentUser from '@/hooks/useCurrentUser';
 import { NextPage } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+
 
 
 export async function getServerSideProps(context: NextPage) {
@@ -25,9 +27,7 @@ export default function Home() {
   const { data: user } = useCurentUser();
   return (
     <>
-      <h1 className='text-2xl text-green-500'>Netflix Clone</h1>
-      <p className='text-white'>Logged in as : {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout;</button>
+      <Navbar />
     </>
   )
 }
